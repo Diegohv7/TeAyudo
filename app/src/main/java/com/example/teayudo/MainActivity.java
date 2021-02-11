648545b31640e30400ce343c5adf981ad3cc0b64
Solution <62267951+Diegohv7@users.noreply.github.com>
2021-02-10 23:55:39 -0400

    xd

--- app/src/main/java/com/example/teayudo/MainActivity.java
+++ app/src/main/java/com/example/teayudo/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     //Hola Mundo
+    //Hola Mundo :D
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
